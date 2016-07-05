b64c19ca24b61755a7587fb0585d7776fd2b87fd
Mihai Ivascu <me@miahi.ro>
2016-07-05 17:20:15 +0300

    Added form-filling sample.

--- src/ro/miahi/pdf/TestSignature.java
+++ src/ro/miahi/pdf/TestSignature.java
@@ -1,13 +1,18 @@
 package ro.miahi.pdf;
 
+import com.lowagie.text.ExceptionConverter;
 import com.lowagie.text.pdf.AcroFields;
 import com.lowagie.text.pdf.PdfPKCS7;
 import com.lowagie.text.pdf.PdfReader;
+import com.lowagie.text.pdf.PdfStamper;
 
+import java.io.FileOutputStream;
 import java.security.*;
 import java.security.cert.Certificate;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Iterator;
 
 public class TestSignature {
 
@@ -18,9 +23,33 @@ public class TestSignature {
             TestSignature ts = new TestSignature();
             ts.setFileName("SampleSignedPDFDocument.pdf");
             ts.verifyPdf();
+            ts.setFileName("OoPdfFormExample.pdf");
+            ts.fillForm();
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
+    }
+
+    public void fillForm() throws Exception {
+        PdfReader reader = new PdfReader(fileName);
+        String outputFileName = fileName + "_out.pdf";
+
+        PdfStamper stamp = new PdfStamper(reader, new FileOutputStream(outputFileName));
+
+        AcroFields form = stamp.getAcroFields();
+
+        HashMap formFields = form.getFields();
+        Iterator iterator = formFields.keySet().iterator();
+        System.out.println("Found fields: ");
+        while (iterator.hasNext()) {
+            String field = (String) iterator.next();
+            System.out.println("  Field >" + field + "<");
+            form.setField(field, "1");
+        }
+
+        stamp.setFormFlattening(true);
+        stamp.close();
+
     }
 
     public boolean verifyPdf() throws Exception {
@@ -67,6 +96,8 @@ public class TestSignature {
             else
                 System.out.println("Certificate verification failed: " + fails[1]);
         }
+
+        reader.close();
         return true;
     }
 
